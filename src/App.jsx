a33da00590fe41476195fcfaf92e80befcb1b7fc
Pallagi Joe <pallagijoe@gmail.com>
2024-04-15 07:30:46 +0200

    Add Profile and WelcomePage components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import { Profile } from "./Profile";
+import { WelcomePage } from "./WelcomePage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
-  
+      {/* use this button to change what is rendered */}
+      <button onChange={null}>Change State</button>
+      {/* render Profile or Compnent here */}
     </>
   );
 }
